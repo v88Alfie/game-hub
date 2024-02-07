3df1a6ae6170ff7b9398b8239fcf4e5bcf06ec41
v88Alfie <aosayan@village88.com>
2024-02-08 00:27:48 +0800

    displaying genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,9 @@
 import useData from './useData';
 
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
-
 export interface Genre {
     id: number;
     name: string;
+    image_background: string;
 }
 
 const useGenres = () => useData<Genre>('/genres');
