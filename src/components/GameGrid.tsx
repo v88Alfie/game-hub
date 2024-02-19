79f451632a9c06f887b33a6bfc0a8a15aed4bf87
v88Alfie <aosayan@village88.com>
2024-02-19 14:57:49 +0800

    refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,6 +13,8 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
     <>
       {error && <Text>{error}</Text>}
